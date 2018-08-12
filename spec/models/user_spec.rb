5bc34b0e8a3211042c3a8e1f872c8d80d41dd9b3
swgoodman <spencerwgoodman@icloud.com>
2018-08-11 21:23:33 -0700

    wrote user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,47 +4,52 @@ RSpec.describe User, :type => :model do
   let(:user) {
     User.create(
       :name => "Mindy",
+      :email => "mindy@gmail.com",
       :password => "password",
-      :nausea => 5,
-      :happiness => 3,
-      :tickets => 4,
-      :height => 34
     )
   }
 
-  let(:admin) {
-    User.create(
-      :name => "Walt",
-      :password => "password",
-      :nausea => 5,
-      :happiness => 3,
-      :tickets => 4,
-      :height => 34,
-      :admin => true
+  let(:library_records) {
+    Library.create(
+      :name => "My Records",
+      :category => "Music",
+      :user_id => 1,
     )
   }
 
-  let(:roller_coaster) {
-    Attraction.create(
-      :name => "Roller Coaster",
-      :tickets => 5,
-      :nausea_rating => 2,
-      :happiness_rating => 4,
-      :min_height => 32
+  let(:library_books) {
+    Library.create(
+      :name => "My Books",
+      :category => "Books",
+      :user_id => 1,
     )
   }
 
-  let(:ferris_wheel) {
-    Attraction.create(
-      :name => "Ferris Wheel",
-      :tickets => 2,
-      :nausea_rating => 2,
-      :happiness_rating => 1,
-      :min_height => 28
+  let(:mastery) {
+    Book.create(
+      :library_id => 1,
+      :name => "Mastery",
+      :author => "Robert Greene",
+      :format => "Physical Copy",
+      :status => "In Progress",
+      :note => "Pg. 28 is interesting.",
+      :category => "Personal Development",
     )
   }
 
-  it "is valid with a name, password, happiness, nausea, height, and tickets" do
+  let(:emerson) {
+    Book.create(
+      :library_id => 1,
+      :name => "The Essays of Ralph Waldo Emerson",
+      :author => "Ralph Waldo Emerson",
+      :format => "Physical Copy",
+      :status => "In Progress",
+      :note => "The Address resonates.",
+      :category => "Philosophy",
+    )
+  }
+
+  it "is valid with a name, email, and password" do
     expect(user).to be_valid
   end
 
@@ -52,34 +57,17 @@ RSpec.describe User, :type => :model do
     expect(User.new(name: "Name")).not_to be_valid
   end
 
-  it "is valid with an admin boolean" do
-    expect(admin).to be_valid
+  it "has many libraries" do
+    first_library = Library.create(:user_id => user.id, :name => library_books.name, :category => "Books")
+    second_library = Library.create(:user_id => user.id, :name => library_records.name, :category => "Records")
+    expect(user.libraries.first).to eq(first_ride)
+    expect(user.libraries.last).to eq(second_ride)
   end
 
-  it "defaults to admin => false" do
-    expect(user.admin).to eq(false)
-  end
-
-  it "has many rides" do
-    first_ride = Ride.create(:user_id => user.id, :attraction_id => roller_coaster.id)
-    second_ride = Ride.create(:user_id => user.id, :attraction_id => ferris_wheel.id)
-    expect(user.rides.first).to eq(first_ride)
-    expect(user.rides.last).to eq(second_ride)
-  end
-
-  it "has many attractions through rides" do
-    user.attractions << [roller_coaster, ferris_wheel]
-    expect(user.attractions.first).to eq(roller_coaster)
-    expect(user.attractions.last).to eq(ferris_wheel)
-  end
-
-  it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
-    expect(user.mood).to eq("sad")
-  end
-
-  it "has a method 'mood' that returns 'happy' when the user is more happy than nauseous" do
-    user.update(:happiness => 7)
-    expect(user.mood).to eq("happy")
+  it "has many books through libraries" do
+    user.books << [mastery, emerson]
+    expect(user.books.first).to eq(mastery)
+    expect(user.books.last).to eq(emerson)
   end
 
 end
