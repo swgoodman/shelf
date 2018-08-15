c76f5a708f9d98ab540f7d040852ce917aa331d7
swgoodman <spencerwgoodman@icloud.com>
2018-08-14 18:14:25 -0700

    implemented session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
+    @users = User.all
   end
 
   def create
+    user = User.find_by_id(params[:id])
+    if !user
+      redirect_to root_path
+    else
+      session[:user_id] = user.idea
+      redirect_to user_path(user)
+    end
   end
 
   def destroy
